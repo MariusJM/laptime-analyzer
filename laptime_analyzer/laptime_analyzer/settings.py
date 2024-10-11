8a5943ea7d3d0c3533955461e04367e9e7a558d1
MariusJM <mariusjm.code@gmail.com>
2024-10-11 23:02:02 +0300

    update settings

--- laptime_analyzer/laptime_analyzer/settings.py
+++ laptime_analyzer/laptime_analyzer/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +79,7 @@ WSGI_APPLICATION = 'laptime_analyzer.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
