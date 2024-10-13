36ef8a811a38a063e28a160f6daa31cdb12448b8
MariusJM <mariusjm.code@gmail.com>
2024-10-14 00:22:52 +0300

    add home pages add home urls add home views

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),  # This connects the root URL to the 'index' view
+    path('naujienos/', views.naujienos, name='naujienos'),
+    path('burelis/', views.burelis, name='burelis'),
+    path('pamokos/', views.pamokos, name='pamokos'),
+    path('kainos/', views.kainos, name='kainos'),
+    path('kontaktai/', views.kontaktai, name='kontaktai'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 def index(request):
     # This function handles what happens when someone visits the home page
     return render(request, 'home/index.html')
+
+def naujienos(request):
+    return render(request, 'home/naujienos.html')
+
+def burelis(request):
+    return render(request, 'home/burelis.html')
+
+def pamokos(request):
+    return render(request, 'home/pamokos.html')
+
+def kainos(request):
+    return render(request, 'home/kainos.html')
+
+def kontaktai(request):
+    return render(request, 'home/kontaktai.html')
